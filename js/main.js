3f06f7fe4d636b9e8a40738129c0f6aef3c5f88f
bcrowell <brettcrowell@gmail.com>
2014-12-02 23:13:38 -0500

    Implementing completely different `getHexAtPoint` method

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var options = {
 
   viewportId: 'viewport',
   sourceCanvasId: 'imgSource',
+  testImage: './img/test-image.jpg',
   imageWidth: 640,
   imageHeight: 480,
   resolution: 0.25,
@@ -10,37 +11,57 @@ var options = {
 }
 
 // http://www.html5canvastutorials.com/advanced/html5-canvas-load-image-data-url/
-function loadCanvas(dataURL) {
-  var canvas = document.getElementById(options.sourceCanvasId);
-  var context = canvas.getContext('2d');
+// http://stackoverflow.com/questions/6735470/get-pixel-color-from-canvas-on-mouseover
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+function getHexAtPoint(x, y, ctx){
+
+  var p = ctx.getImageData(x, y, 1, 1).data,
+      hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+
+  return hex;
+
+}
+
+
+$(document).ready(function(){
+
+  var canvas = document.getElementById(options.sourceCanvasId),
+      context = canvas.getContext('2d');
 
   // load image from data url
   var imageObj = new Image();
   imageObj.onload = function() {
+
     context.drawImage(this, 0, 0);
-  };
 
-  imageObj.src = dataURL;
-}
+    var viewport = $('#' + options.viewportId),
+        rows = options.imageHeight * options.resolution,
+        cols = options.imageWidth * options.resolution;
 
-$(document).ready(function(){
+    for(var r = 0; r < rows; r++){
 
-  loadCanvas("./img/test-image.jpg")
+      for(var c = 0; c < cols; c++){
 
-  var viewport = $('#' + options.viewportId),
-      rows = options.imageHeight * options.resolution,
-      cols = options.imageWidth * options.resolution;
+        var xOnCanvas = c * (1 / options.resolution),
+            yOnCanvas = r * (1 / options.resolution);
 
-  for(var r = 0; r < rows; r++){
+        var colorAtPoint = getHexAtPoint(xOnCanvas, yOnCanvas, context);
 
-    for(var c = 0; c < cols; c++){
+        viewport.append("<span style='color:" + colorAtPoint + "'>" + options.pixelChar + "</span>");
 
-      viewport.append(options.pixelChar);
+      }
+
+      viewport.append("<br/>");
 
     }
 
-    viewport.append("<br/>");
+  };
 
-  }
+  imageObj.src = options.testImage;
 
 });
