5fb757c064dcba1b9e5f23167528f25e11925a09
bcrowell <brettcrowell@gmail.com>
2014-12-02 21:59:16 -0500

    Very early grid implementation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+var options = {
 
+  viewportId: 'viewport',
+  imageWidth: 640,
+  imageHeight: 480,
+  scaleFactor: 0.25,
+  pixelChar: '•'
+
+}
+
+$(document).ready(function(){
+
+  var viewport = $('#' + options.viewportId),
+      rows = options.imageHeight * options.scaleFactor,
+      cols = options.imageWidth * options.scaleFactor;
+
+  for(var r = 0; r < rows; r++){
+
+    for(var c = 0; c < cols; c++){
+
+      viewport.append(options.pixelChar);
+
+    }
+
+    viewport.append("<br/>");
+
+  }
+
+});
