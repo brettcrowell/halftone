df09dcdb407c37252765f70aabbaf1a6a4c6259b
bcrowell <brettcrowell@gmail.com>
2014-12-02 22:37:48 -0500

    Loading test image onto `canvas`

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var options = {
 
   viewportId: 'viewport',
+  sourceCanvasId: 'imgSource',
   imageWidth: 640,
   imageHeight: 480,
   resolution: 0.25,
@@ -8,7 +9,22 @@ var options = {
 
 }
 
+function loadCanvas(dataURL) {
+  var canvas = document.getElementById(options.sourceCanvasId);
+  var context = canvas.getContext('2d');
+
+  // load image from data url
+  var imageObj = new Image();
+  imageObj.onload = function() {
+    context.drawImage(this, 0, 0);
+  };
+
+  imageObj.src = dataURL;
+}
+
 $(document).ready(function(){
+
+  loadCanvas("./img/test-image.jpg")
 
   var viewport = $('#' + options.viewportId),
       rows = options.imageHeight * options.resolution,
