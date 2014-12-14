caeea203051b5cc39466923be6c071bd949aea28
bcrowell <brettcrowell@gmail.com>
2014-12-14 13:24:47 -0500

    Increasing source resolution

--- js/Bullet/Options.js
+++ js/Bullet/Options.js
@@ -6,14 +6,15 @@ Bullet.Options = {
     svgNamespace: "http://www.w3.org/2000/svg",
     testImage: './img/test-image.jpg',
     quality: 120,
+    stagger: true,
     minPixelSimilarity: 0.95,
     frameRate: 8,
     backgroundColor: '#eee',
     videoConstraints: {
         video: {
             mandatory: {
-                maxWidth: 160,
-                maxHeight: 120
+                maxWidth: 320,
+                maxHeight: 240
             }
         }
     }
