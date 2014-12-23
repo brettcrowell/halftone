bfb21b7ebddc48f8abca5bceace41c90065d097c
bcrowell <brettcrowell@gmail.com>
2014-12-22 21:06:47 -0500

    More similarity adjustments

--- js/Bullet/Options.js
+++ js/Bullet/Options.js
@@ -5,10 +5,10 @@ Bullet.Options = {
     sourceCanvasId: 'imgSource',
     svgNamespace: "http://www.w3.org/2000/svg",
     testImage: './img/test-image.jpg',
-    quality: 120,
+    quality: 80,
     colorMultiplier: 1.7,
     stagger: true,
-    hueSimilarity: 0.5,
+    hueSimilarity: 0.001,
     frameRate: 7,
     backgroundColor: '#eee',
     videoConstraints: {
