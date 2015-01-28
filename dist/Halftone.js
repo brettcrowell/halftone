c74189d1e7b97a6de87b41cf268a61c87f54fa4a
bcrowell <brettcrowell@gmail.com>
2015-01-27 20:48:32 -0500

    Playing with Options

--- dist/Halftone.js
+++ dist/Halftone.js
@@ -27,11 +27,11 @@ Halftone.Options = {
     sourceCanvasId: 'imgSource',
     svgNamespace: "http://www.w3.org/2000/svg",
     testImage: './img/test-image.jpg',
-    quality: 150,
+    quality: 100,
     pixelSize: 10,
     aspectRatio: 16 / 9,
-    colorMultiplier: 3,
-    colorBase: 16, // max 36
+    colorMultiplier: 1,
+    colorBase: 10, // max 36
     stagger: true,
     maxPctRgbDifference: 0.02,
     maxDeltaE: 10,
