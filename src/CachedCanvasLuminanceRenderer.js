3a7bc76d0c0ecd0fda4058d65e251a958ca5922e
bcrowell <brettcrowell@gmail.com>
2015-08-29 00:13:37 -0400

    autoPixelSize

--- src/CachedCanvasLuminanceRenderer.js
+++ src/CachedCanvasLuminanceRenderer.js
@@ -3,6 +3,7 @@ Halftone.CachedCanvasLuminanceRenderer = function(options){
     this.options = {
 
         pixelSize: 20,
+        autoPixelSize: true,
         invert: true
 
     };
@@ -55,11 +56,23 @@ Halftone.CachedCanvasLuminanceRenderer.prototype = {
 
     },
 
-    getCachedPixel: function(luminance){
+    getCachedPixel: function(luminance, pixelSize){
 
-        this.cache[luminance] = this.cache[luminance] || this.generateCircle(luminance, this.options.pixelSize);
+        var cachedPixel;
 
-        return this.cache[luminance];
+        if(this.cache[pixelSize]){
+
+            cachedPixel = this.cache[pixelSize][luminance];
+
+        } else {
+
+            this.cache[pixelSize] = [];
+
+        }
+
+        this.cache[pixelSize][luminance] = cachedPixel || this.generateCircle(luminance, pixelSize);
+
+        return this.cache[pixelSize][luminance];
 
     },
 
@@ -70,12 +83,20 @@ Halftone.CachedCanvasLuminanceRenderer.prototype = {
             rows = encoderOutput.metadata.rows;
 
         var pixelSize = this.options.pixelSize + (this.options.pixelSize % 2);
-        var pixelRadius = pixelSize / 2;
 
         if(this.element.width != (cols * pixelSize)){
+
+            if(this.options.autoPixelSize){
+                pixelSize = this.options.pixelSize = parseInt(this.element.offsetWidth / cols, 10);
+                pixelSize += pixelSize % 2;
+            }
+
             this.element.width = cols * pixelSize;
             this.element.height = rows * pixelSize;
+
         }
+
+        var pixelRadius = pixelSize / 2;
 
         for(var luminance in matrix) {
 
@@ -83,7 +104,7 @@ Halftone.CachedCanvasLuminanceRenderer.prototype = {
 
             var context = this.element.getContext('2d');
 
-            var sourcePixel = this.getCachedPixel(luminance);
+            var sourcePixel = this.getCachedPixel(luminance, pixelSize);
 
             var row, col, xOffset;
 
